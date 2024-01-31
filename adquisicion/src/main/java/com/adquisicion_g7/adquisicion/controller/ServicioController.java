ed8f6490937d97d11fdf087ac1ab33b36f2524db
ale <alesaidon@gmail.com>
2024-01-31 08:29:21 -0300

    ServicioController

---
+++ adquisicion/src/main/java/com/adquisicion_g7/adquisicion/controller/ServicioController.java
@@ -0,0 +1,61 @@
+package com.adquisicion_g7.adquisicion.controller;
+
+import com.adquisicion_g7.adquisicion.dto.MensajeDTO;
+import com.adquisicion_g7.adquisicion.dto.ServicioDTO;
+import com.adquisicion_g7.adquisicion.entities.Servicio;
+import com.adquisicion_g7.adquisicion.service.ServicioService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/servicio")
+public class ServicioController {
+
+    private final ServicioService servicioService;
+
+    @Autowired
+    public ServicioController(ServicioService servicioService) {
+        this.servicioService = servicioService;
+    }
+
+    @PostMapping("/guardar")
+    public ResponseEntity<MensajeDTO> guardarServicio(@RequestBody @Valid ServicioDTO servicioDTO) {
+        MensajeDTO mensajeDTO = servicioService.guardarServicio(servicioDTO);
+        return ResponseEntity.ok(mensajeDTO);
+    }
+
+    @DeleteMapping("/borrar/{id}")
+    public ResponseEntity<Void> removerServicio(@PathVariable Long id) {
+        servicioService.removerServicio(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/nombre/{nombre}")
+    public  ResponseEntity<MensajeDTO> buscarPorNombreTipoServicio(@PathVariable String nombre) {
+        MensajeDTO mensajeDTO = servicioService.buscarNombreTipoServicio(nombre);
+        return ResponseEntity.ok(mensajeDTO);
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<Servicio>> listarTodas() {
+        return ResponseEntity.ok(servicioService.listarTodas());
+
+    }
+    @DeleteMapping("/eliminarPorId/{id}")
+    public ResponseEntity<MensajeDTO> eliminarServcioPorId(@PathVariable Long id) {
+        MensajeDTO mensajeDTO = servicioService.eliminarServicioPorId(id);
+        return ResponseEntity.ok(mensajeDTO);
+    }
+    @PutMapping("/recuperarPorId/{id}")
+    public ResponseEntity<MensajeDTO> recuperarServicioPorId(@PathVariable Long id) {
+        MensajeDTO mensajeDTO = servicioService.recuperarServicioPorId(id);
+        return ResponseEntity.ok(mensajeDTO);
+
+    }
+
+
+}
