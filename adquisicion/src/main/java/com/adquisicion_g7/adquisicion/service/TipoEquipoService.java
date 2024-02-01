375e5f6ca9d970f7c427e7a4e7a9986be7a5e9ba
Juan Vicente <juamar1306@hotmail.com>
2024-02-01 20:47:26 -0300

    cambios

--- adquisicion/src/main/java/com/adquisicion_g7/adquisicion/service/TipoEquipoService.java
+++ adquisicion/src/main/java/com/adquisicion_g7/adquisicion/service/TipoEquipoService.java
@@ -1,6 +1,7 @@
 package com.adquisicion_g7.adquisicion.service;
 
 import com.adquisicion_g7.adquisicion.entities.TipoEquipo;
+import com.adquisicion_g7.adquisicion.entities.TipoMaterial;
 import com.adquisicion_g7.adquisicion.repository.TipoEquipoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,5 +16,13 @@ public class TipoEquipoService {
     @Autowired
     public TipoEquipoService(TipoEquipoRepository tipoEquipoRepository) {
         this.tipoEquipoRepository = tipoEquipoRepository;
+    }
+
+    public Optional<TipoEquipo> buscarPorId(Long id) {
+        return tipoEquipoRepository.findById(id);
+    }
+
+    public TipoEquipo guardar(TipoEquipo tipoEquipo) {
+        return tipoEquipoRepository.save(tipoEquipo);
     }
 }
